03340149d3447b2531ad486902baa704b2177d60
Vladimir Prokhnenko <volmir@ukr.net>
2018-01-22 18:47:59 +0200

    Add project files

---
+++ src/notfound.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        404 Not found
+      </button>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/rules.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class Rules extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        sdfgsdgfd
+      </button>
+    );
+  }
+}
+
+export default Rules;
+
